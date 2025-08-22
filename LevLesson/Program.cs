1b239e25e9e697ec939ab66ea39add24a6a67bb6
Alestln <itworkplace46@gmail.com>
2025-08-22 14:08:20 +0300

    Text output options. Commetaries.

--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -4,12 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Console.WriteLine("Hello, World!");
             Console.WriteLine("This is a simple C# program to demonstrate basic syntax.");
             Console.WriteLine("Press any key to exit...");
 
             // Wait for user input before closing the console window
-            Console.ReadKey();
+            Console.ReadKey();*/
+
+            Console.Write("Hello! My name is Alex.");
+
+            /*Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();*/
+
+            /*
+             * Какой-то текст
+             * Продолжение текста
+             * 
+             * 
+             */
+
+            Console.WriteLine("\n\tHello! My name is Alex.");
         }
     }
 }
