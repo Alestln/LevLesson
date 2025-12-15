5ee8ff69af3069b5ca0019b1ca579e6c03f89a85
Alestln <itworkplace46@gmail.com>
2025-12-15 14:44:07 +0200

    Done

--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
+            // Массивы
+            // Создание массива
+            // int[] arr = new int[10];
             
+            // Инициализация массива в коде
+            // int[] arr = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // Обращение к элементу массива по индексу
+            // arr[0]
+            
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Random.Shared.Next(1, 10);
+            }
+
+            // Почитать про методы Array
+            // Array.Resize(ref arr, 30);
         }
     }
 }
