e18b5582486dddfd62748a1564bbed813e61f09d
Alestln <itworkplace46@gmail.com>
2026-01-23 13:15:33 +0200

    Done

--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
+            // Тема занятия: Двумерные массива
+            int[] arr = new int[5] { 5, 4, 7, 6, 8 }; // Одномерный массив
             
+            // Создать двумерный массив
+            // int[,] matrix = new int[5, 4];
+            
+            // Инициализация двумерного массива в коде
+            /*int[,] matrix = new int[2, 3]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 }
+            };*/
+
+            int[,] matrix = new int[5, 4];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = Random.Shared.Next(1, 1000);
+                }
+            }
+
+            Console.WriteLine("Double array:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
