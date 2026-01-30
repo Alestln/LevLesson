3131315596f3463fe6a1e20fdad4255050c45d07
Alestln <itworkplace46@gmail.com>
2026-01-30 13:49:49 +0200

    Done

--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Start program");
             
+            // Методы
+            Method();
+            Method("Text from parameter");
+
+            // Перегрузка метода
+            int[,] matrix = CreateDoubleArray(2, 3);
+            
+            // Сигнатура метода
+            // В сигнатуру метода входит название метода, количество аргументов метода, порядок аргументов и типы аргументов
+            
+            Console.WriteLine("End program");
+        }
+
+        static int[,] CreateDoubleArray(int size)
+        {
+            int[,] array = new int[size, size];
+
+            return array;
+        }
+        
+        static int[,] CreateDoubleArray(int rows, int columns)
+        {
+            return new int[rows, columns];
+        }
+        
+        static void Method()
+        {
+            Console.WriteLine("Method works!");
+
+            // Под капотом вызовется этот return
+            return;
+        }
+        
+        static void Method(string text)
+        {
+            Console.WriteLine(text);
         }
     }
 }
