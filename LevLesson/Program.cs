e07f00767be1e0735997b4928603a7228f7daa33
Alestln <itworkplace46@gmail.com>
2026-03-06 13:34:16 +0200

    Done

--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
+            int a = 5;
+            char symbol = '$';
+            string text = "Hello world";
+            text = "Hello";
+            string copyText = text;
+
+            int b = 10;
+            int result = Sum(a, b, out int res);
+            Console.WriteLine(result);
             
+            (int Sum, int UniversalNumber) sumResult = Sum(a, b);
+            Console.WriteLine($"Sum: {sumResult.Sum}; Universal Number: {sumResult.UniversalNumber}");
+        }
+
+        static int Sum(int a, int b, out int result)
+        {
+            result = a + b;
+            return a + b;
+        }
+        
+        static (int, int) Sum(int a, int b)
+        {
+            return (a + b, 42);
         }
     }
 }
