52b3400f72e0aa39fae3eea35d03aebce4717c92
Alestln <itworkplace46@gmail.com>
2026-05-29 13:07:11 +0300

    Done

--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace LevLesson
+﻿using System.Text.RegularExpressions;
+
+namespace LevLesson
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string badText = "Привет     мир!   Как   дела?   ";
+
+            string goodText = Regex.Replace(badText, @"\s{2,}", " ");
         }
     }
 }
