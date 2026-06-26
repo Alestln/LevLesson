3dc2e6cb20b70ed40de126c9b9b6f13b670b1cae
Alestln <itworkplace46@gmail.com>
2026-06-26 13:55:02 +0300

    Done

---
+++ LevLesson/Person.cs
@@ -0,0 +1,41 @@
+﻿namespace LevLesson
+{
+    internal class Person
+    {
+        private string _name;
+        private int _age;
+        private double _height;
+
+        internal int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 1 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("Age must be between 1 and 120.");
+                }
+
+                _age = value;
+            }
+        }
+
+        internal char FirstLetterOfName
+        {
+            get
+            {
+                return _name[0];
+            }
+        }
+
+        public Person(string name, int age, double height)
+        {
+            _name = name;
+            _age = age;
+            _height = height;
+        }
+    }
+}
--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            Person person = new Person("Alex", 25, 1.79);
+
+            Console.WriteLine(person.Age);
+            Console.WriteLine(person.FirstLetterOfName);
+
+            try
+            {
+                person.Age = 30;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine(person.Age);
         }
     }
 }
