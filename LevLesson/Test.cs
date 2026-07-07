fbbe8c72785405755d0dbee1abad8ef328c24ae0
Alestln <itworkplace46@gmail.com>
2026-07-07 13:14:05 +0300

    Done

---
+++ LevLesson/Test.cs
@@ -0,0 +1,43 @@
+﻿namespace LevLesson
+{
+    internal class Test
+    {
+        static internal int StaticValue = InitStaticField();
+
+        internal int instanceField = 67;
+
+        internal int Property { get; private init; }
+
+        private int CalculateValue()
+        {
+            return Random.Shared.Next();
+        }
+
+        static Test()
+        {
+            Console.WriteLine("Вызов статического конструктора.");
+
+            // instanceField = 43; // Ошибка компиляции: нельзя обращаться к нестатическим полям в статическом конструкторе
+        }
+
+        internal Test()
+        {
+            Console.WriteLine("Вызов обычного конструктора.");
+            instanceField = InitInstanceField();
+            Property = CalculateValue();
+        }
+
+        private int InitInstanceField()
+        {
+            Console.WriteLine("Инициализация обычного поля класса");
+            return 42;
+        }
+
+        internal static int InitStaticField()
+        {
+            // instanceField = 43; // Ошибка компиляции: нельзя обращаться к нестатическим полям в статическом методе
+            Console.WriteLine("Инициализация статического поля класса");
+            return 100;
+        }
+    }
+}
