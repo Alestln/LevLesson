2123f8079384842699d488868640c667aec30f72
Alestln <itworkplace46@gmail.com>
2026-07-21 13:28:21 +0300

    Done

--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            
+            // Домашнее задание:
+            // Почитать про Очередь (Queue). Реализовать свой класс MyQueue, который будет работать по принципу FIFO (First In First Out).
+            // Реализовать методы Enqueue, Dequeue, Peek, Count, Capacity. Внутри использовать массив для хранения элементов.
+
+            // 1 Не используя Список
+
+            /*string[] names =
+            {
+                "Alex",
+                "Sasha",
+                "Konny",
+                "Alice"
+            };
+
+            string newName = "Lev";
+
+            string[] temp = new string[5];
+            for (int i = 0; i < names.Length; i++)
+            {
+                temp[i] = names[i];
+            }
+
+            temp[temp.Length - 1] = newName;
+
+            names = temp;*/
+
+            // 2 Используя Список
+            List<string> names = new List<string>()
+            {
+                "Alex",
+                "Sasha",
+                "Konny",
+                "Alice"
+            };
+
+            names.Add("Lev");
+
+            string[] copy = new string[5];
+            names.CopyTo(copy);
+
+            names.EnsureCapacity(5);
         }
     }
 }
