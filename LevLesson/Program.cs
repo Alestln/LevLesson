d105bbdcb10465a40bd05569e36ea8fb5fd20a79
Alestln <itworkplace46@gmail.com>
2026-07-24 12:14:50 +0300

    test

--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            
+            Test test = new Test();
+        }
+    }
+
+    public class Test
+    {
+        private int _count;
+
+        public int Count => _count;
+
+        public Test()
+        {
+            Method();   
+        }
+
+        public void Method()
+        {
+            Console.WriteLine("Hello");
         }
     }
 }
