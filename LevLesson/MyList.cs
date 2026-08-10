998bcad80a0bc6f396ac481cae0495d5647ca027
Alestln <itworkplace46@gmail.com>
2026-08-10 13:17:42 +0300

    Done

---
+++ LevLesson/MyList.cs
@@ -0,0 +1,39 @@
+﻿namespace LevLesson
+{
+    public class MyList
+    {
+        private int[] _items;
+
+        public MyList(int[] values)
+        {
+            _items = new int[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                _items[i] = values[i];
+            }
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is MyList otherList)
+            {
+                if (_items.Length != otherList._items.Length)
+                {
+                    return false;
+                }
+
+                for (int i = 0; i < _items.Length; i++)
+                {
+                    if (_items[i] != otherList._items[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
