db9248fe520aead17245a160453000d0da72ccaf
Alestln <itworkplace46@gmail.com>
2026-08-18 13:01:28 +0300

    Done

--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace LevLesson
+﻿using LevLesson.Test;
+
+namespace LevLesson
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Player player1 = new Player("Alice");
+            Player player2 = new Player("Alice");
+
+            bool isEqual = player1.Equals(player2);
+            Console.WriteLine($"Are the players equal? {isEqual}");
+
+            string text = player1.ToString();
+            Console.WriteLine(text);
+
+            Console.WriteLine(player1.GetHashCode());
+
+            // Домашнее задание:
+            // 1. Почитать про абстракцию.
+            // 2. Почитать про интерфейсы. Чем отличаются интерфейсы от наследования. Привести примеры.
         }
+    }
+
+    public struct PlayerInput
+    {
+        public bool IsJump { get; set; }
     }
 }
---
+++ LevLesson/Test/Player.cs
@@ -0,0 +1,42 @@
+﻿namespace LevLesson.Test
+{
+    internal class Player
+    {
+        private string _name;
+        private Movement _movement;
+
+        public Player(string name)
+        {
+            _movement = new Movement();
+            _name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Player otherPlayer)
+            {
+                return _name == otherPlayer._name;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"Player Name: {_name}";
+        }
+
+        /*public override int GetHashCode()
+        {
+            return _name.GetHashCode();
+        }*/
+
+        private class Movement
+        {
+            public void Move()
+            {
+                Console.WriteLine("Player is moving.");
+            }
+        }
+    }
+}
