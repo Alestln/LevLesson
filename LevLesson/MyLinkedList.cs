eddd85ed1d21e7e5017a293743501f8cf24e3673
Alestln <itworkplace46@gmail.com>
2026-09-08 13:33:07 +0300

    Done

---
+++ LevLesson/MyLinkedList.cs
@@ -0,0 +1,125 @@
+﻿public class MyLinkedList<T>
+{
+    private Node<T>? _head;   // первый элемент списка
+    private Node<T>? _tail;   // последний элемент списка
+    private int _count;      // количество элементов
+
+    // Внутренний класс узла
+    private class Node<U>
+    {
+        public U Value;
+        public Node<U>? Next;
+
+        public Node(U value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    // Добавить элемент в конец
+    public void AddLast(T value)
+    {
+        Node<T> newNode = new Node<T>(value);
+
+        if (_head is null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            _tail!.Next = newNode;
+            _tail = newNode;
+        }
+
+        _count++;
+    }
+
+    // Добавить элемент в начало
+    public void AddFirst(T value)
+    {
+        Node<T> newNode = new Node<T>(value);
+
+        newNode.Next = _head;
+        _head = newNode;
+
+        if (_tail is null)
+            _tail = newNode;
+
+        _count++;
+    }
+
+    // Удалить первый элемент
+    public void RemoveFirst()
+    {
+        if (_head is null)
+            throw new InvalidOperationException("Список пуст");
+
+        _head = _head.Next;
+
+        if (_head is null)
+            _tail = null;
+
+        _count--;
+    }
+
+    // Проверка наличия элемента
+    public bool Contains(T value)
+    {
+        Node<T>? current = _head;
+
+        while (current is not null)
+        {
+            if (current.Value is not null && current.Value.Equals(value))
+                return true;
+
+            current = current.Next;
+        }
+
+        return false;
+    }
+
+    // Количество элементов
+    public int Count => _count;
+
+    // Перебор элементов
+    public IEnumerable<T> AsEnumerable()
+    {
+        Node<T>? current = _head;
+
+        while (current is not null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
+    }
+
+    public static bool operator ==(MyLinkedList<T> left, MyLinkedList<T> right)
+    {
+        if (left is null || right is null)
+        {
+            return false;
+        }
+
+        if (left.Count != right.Count)
+        {
+            return false;
+        }
+
+        foreach (var item in left.AsEnumerable())
+        {
+            if (!right.Contains(item))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool operator !=(MyLinkedList<T> left, MyLinkedList<T> right)
+    {
+        return !(left == right);
+    }
+}
--- LevLesson/Program.cs
+++ LevLesson/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            MyLinkedList<int> list1 = new MyLinkedList<int>();
+            list1.AddLast(1);
+            list1.AddLast(3);
+
+            MyLinkedList<int> list2 = new MyLinkedList<int>();
+            list2.AddLast(2);
+            list2.AddLast(4);
+
+            Console.WriteLine(list1 == list2);
+
+            // Домашнее задание
+            // Дописать MyLinkedList<T>. Переопределить операторы сравнения == и !=.
+            // Переопределить индексатор, чтобы можно было получать и устанавливать элементы по индексу. И пройтись по списку с помощью for.
+            // Переопределить оператор + для объединения двух списков и оператор - для удаления элементов одного списка из другого.
         }
     }
 }
